abf2e2d0a074aadd81f5b07c218d65d26389de54
foreverbog <133509015+foreverbog@users.noreply.github.com>
2024-03-27 17:28:26 +0100

    some data

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const url = "http://hn.algolia.com/api/v1/search?query=...";
+  const [news, setNews] = useState([]);
+  useEffect(() => {
+    const fetchData = () => {
+      fetch(url)
+        .then((respone) => respone.json())
+        .then((data) => {
+          console.log(data.hits);
+          // console.log(data.hits[11].author);
+          setNews(data.hits);
+        })
+        .catch((error) => console.error(error));
+    };
+    fetchData();
+  }, []);
+  const calculateTimeAgo = (createdAt) => {
+    const createdDate = new Date(createdAt * 1000); // Convert to milliseconds
+    const currentDate = new Date();
+    const timeDifference = Math.abs(currentDate - createdDate); // Difference in milliseconds
+    const hoursDifference = Math.floor(timeDifference / (1000 * 60 * 60)); // Convert milliseconds to hours
+    return `${hoursDifference} hours ago`;
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Hacker News</h1>
+      {news.map((news) => (
+        <div key={Math.random()}>
+          <h2 key={news.title}>{news.title}</h2>
+          <p key={news.points}>
+            {news.points} points by {news.author}{" "}
+            {calculateTimeAgo(news.created_at_i)} <button>Hide</button>{" "}
+            {news.num_comments} comments
+          </p>
+        </div>
+      ))}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
